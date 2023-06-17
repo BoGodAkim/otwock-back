e92ba56d3b92b0d8fdfb6c10dd56a6fd7a3187d0
Akim Bahadziazh <bogakim@gmail.com>
2023-06-17 19:18:30 +0200

    w

--- src/app.controller_app.ts
+++ src/app.controller_app.ts
@@ -1,32 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { randomUUID } from 'crypto';
-import { AlertService } from "./database/alert/alert.service";
-import { NotificationAddressService } from "./database/notification_address/notification_address.service";
-import { DeviceService } from "./database/device/device.service";
+import { AlertService } from './database/alert/alert.service';
+import { NotificationAddressService } from './database/notification_address/notification_address.service';
+import { DeviceService } from './database/device/device.service';
 import { NotificationAddress } from './database/notification_address/notification_address.entity';
 import { Device } from './database/device/device.entity';
 import { Circle } from './database/circle';
 import { Notification } from './database/notification/notification.entity';
 
-
 @Controller('app')
 export class AppControllerApp {
-  constructor(private readonly alertService: AlertService,
+  constructor(
+    private readonly alertService: AlertService,
     private readonly notificationAddressService: NotificationAddressService,
-    private readonly deviceService: DeviceService) { }
+    private readonly deviceService: DeviceService,
+  ) {}
 
   private async checkDevice(id: string): Promise<boolean> {
-    let device = await this.deviceService.findOne(id);
-    if (device == null) {
-      return false;
-    }
-    return true;
+    const device = await this.deviceService.findOne(id);
+    return device != null;
   }
 
   @Get('/id')
   async getId(): Promise<string> {
-    let uuid = randomUUID();
-    let device = new Device();
+    const uuid = randomUUID();
+    const device = new Device();
     device.id = uuid;
     await this.deviceService.create(device);
     return uuid;
@@ -34,48 +40,61 @@ export class AppControllerApp {
 
   @Get('/:id/myAddresses')
   async getMyAddresses(@Param('id') id: string): Promise<void> {
-    if (!await this.checkDevice(id)) {
+    if (!(await this.checkDevice(id))) {
       return;
     }
-    (await (this.deviceService.findOne(id))).notificationAddresses;
+    (await this.deviceService.findOne(id)).notificationAddresses;
   }
 
   @Post('/:id/address')
-  async addAddress(@Param('id') id: string, @Body() address: NotificationAddress): Promise<void> {
-    if (!await this.checkDevice(id)) {
+  async addAddress(
+    @Param('id') id: string,
+    @Body() address: NotificationAddress,
+  ): Promise<void> {
+    if (!(await this.checkDevice(id))) {
       return;
     }
-    this.notificationAddressService.create(address);
+    await this.notificationAddressService.create(address);
   }
 
   @Delete('/:id/address/:addressId')
-  async deleteAddress(@Param('id') id: string, @Param('addressId') addressId: number): Promise<void> {
-    if (!await this.checkDevice(id)) {
+  async deleteAddress(
+    @Param('id') id: string,
+    @Param('addressId') addressId: number,
+  ): Promise<void> {
+    if (!(await this.checkDevice(id))) {
       return;
     }
-    this.notificationAddressService.delete(addressId);
+    await this.notificationAddressService.delete(addressId);
   }
 
   @Put('/:id/address/:addressId')
-  async updateAddress(@Param('id') id: string, @Param() addressId: number, @Body() address: NotificationAddress): Promise<void> {
-    if (!await this.checkDevice(id)) {
+  async updateAddress(
+    @Param('id') id: string,
+    @Param() addressId: number,
+    @Body() address: NotificationAddress,
+  ): Promise<void> {
+    if (!(await this.checkDevice(id))) {
       return;
     }
     address.id = addressId;
-    this.notificationAddressService.update(address);
+    await this.notificationAddressService.update(address);
   }
 
   @Put('/:id/updateLocation')
-  async shareLocation(@Param('id') id: string, @Body() location: Circle): Promise<void> {
-    if (!await this.checkDevice(id)) {
+  async shareLocation(
+    @Param('id') id: string,
+    @Body() location: Circle,
+  ): Promise<void> {
+    if (!(await this.checkDevice(id))) {
       return;
     }
-    this.deviceService.updateLocation(id, location);
+    await this.deviceService.updateLocation(id, location);
   }
 
   @Get('/:id/notification')
   async getMyNotifications(@Param('id') id: string): Promise<Notification[]> {
-    if (!await this.checkDevice(id)) {
+    if (!(await this.checkDevice(id))) {
       return;
     }
     return (await this.deviceService.findOne(id)).notifications;
