3543bac1318a78b0d503f3c15cbe4796033b1935
Akim Bahadziazh <bogakim@gmail.com>
2023-06-17 19:28:29 +0200

    w

--- src/database/device/device.service.ts
+++ src/database/device/device.service.ts
@@ -1,29 +1,31 @@
-import {Inject, Injectable, Param} from "@nestjs/common";
-import { Repository } from "typeorm";
-import { Device } from "./device.entity";
-import { Circle } from "../circle";
+import { Inject, Injectable, Param } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Device } from './device.entity';
+import { Circle } from '../circle';
 
 @Injectable()
 export class DeviceService {
-    constructor(
-        @Inject('DEVICE_REPOSITORY')
-        private deviceRepository:
-        Repository<Device>,
-    ) { }
+  constructor(
+    @Inject('DEVICE_REPOSITORY')
+    private deviceRepository: Repository<Device>,
+  ) {}
 
-    async create(device: Device): Promise<Device> {
-        return this.deviceRepository.save(device);
-    }
+  async create(device: Device): Promise<Device> {
+    return this.deviceRepository.save(device);
+  }
 
-    async findOne(@Param() id: string): Promise<Device> {
-        return this.deviceRepository.findOne({where:{id:id}, relations: {notificationAddresses:true}})
-        //return this.deviceRepository.findOneBy({ id: id, relations: {notificationAddresses:true}});
-    }
+  async findOne(@Param() id: string): Promise<Device> {
+    return this.deviceRepository.findOne({
+      where: { id: id },
+      relations: { notificationAddresses: true, notifications: true },
+    });
+    //return this.deviceRepository.findOneBy({ id: id, relations: {notificationAddresses:true}});
+  }
 
-    async updateLocation(id: string, location: Circle): Promise<void> {
-        let device = await this.deviceRepository.findOneBy({ id: id });
-        device.lastCoordinates = location;
-        device.timestamp = new Date();
-        this.deviceRepository.save(device);
-    }
+  async updateLocation(id: string, location: Circle): Promise<void> {
+    const device = await this.deviceRepository.findOneBy({ id: id });
+    device.lastCoordinates = location;
+    device.timestamp = new Date();
+    await this.deviceRepository.save(device);
+  }
 }
