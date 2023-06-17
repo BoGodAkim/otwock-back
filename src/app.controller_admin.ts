e0c1fc631541bb27b0017a750af6436a27133bec
Akim Bahadziazh <bogakim@gmail.com>
2023-06-17 19:20:50 +0200

    w

--- src/app.controller_admin.ts
+++ src/app.controller_admin.ts
@@ -1,29 +1,36 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
-import {AlertService} from "./database/alert/alert.service";
-import {Alert} from "./database/alert/alert.entity";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { AlertService } from './database/alert/alert.service';
+import { Alert } from './database/alert/alert.entity';
 
 @Controller('admin')
 export class AppControllerAdmin {
-    constructor(private readonly alertService: AlertService) {}
+  constructor(private readonly alertService: AlertService) {}
 
+  @Post('/alert')
+  addAlert(@Body() alert: Alert) {
+    return this.alertService.create(alert);
+  }
 
-    @Post('/alert')
-    addAlert(@Body() alert: Alert) {
-        return this.alertService.create(alert);
-    }
+  @Put('/alert')
+  updateAlert(@Body() alert: Alert) {
+    return this.alertService.update(alert);
+  }
 
-    @Put('/alert')
-    updateAlert(@Body() alert: Alert) {
-        return this.alertService.update(alert);
-    }
+  @Delete('/alert/:id')
+  deleteAlert(@Param('id') id: number) {
+    return this.alertService.delete(id);
+  }
 
-    @Delete('/alert/:id')
-    deleteAlert(@Param('id') id: number) {
-        return this.alertService.delete(id);
-    }
-
-    @Get('/alert')
-    getAllAlerts(): Promise<Alert[]> {
-        return this.alertService.findAll();
-    }
+  @Get('/alert')
+  getAllAlerts(): Promise<Alert[]> {
+    return this.alertService.findAll();
+  }
 }
