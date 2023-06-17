3c09d8479a55d68862b45f4226da2270b68c6e27
Akim Bahadziazh <bogakim@gmail.com>
2023-06-17 13:43:32 +0200

    db

--- src/database/area/area.entity.ts
+++ src/database/area/area.entity.ts
@@ -1,6 +1,47 @@
-import { Entity } from "typeorm";
+import { Column, Entity, Polygon, PrimaryGeneratedColumn } from "typeorm";
+import { Circle, circleTransformer } from "../circle";
 
 
 @Entity()
 export class Area {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        nullable: true,
+    })
+    polygon: Polygon;
+
+    @Column({
+        type: 'circle',
+        nullable: true,
+        transformer: circleTransformer,
+    })
+    circle: Circle;
+
+    @Column({
+        nullable: true,
+    })
+    country: string;
+
+    @Column({
+        nullable: true,
+    })
+    city: string;
+
+    @Column({
+        nullable: true,
+    })
+    postalCode: string;
+
+    @Column({
+        nullable: true,
+    })
+    street: string;
+
+    @Column({
+        nullable: true,
+    })
+    houseNumber: string;
+
 }
