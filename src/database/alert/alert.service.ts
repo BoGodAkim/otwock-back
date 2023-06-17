85e6a2d506eb806349689e76c22a57554421ca9b
Timur <timur3664.tg@gmail.com>
2023-06-17 13:56:14 +0200

    db

---
+++ src/database/alert/alert.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { Alert } from "./alert.entity";
+
+@Injectable()
+export class AlertService {
+    private readonly alerts: Alert[] = [];
+
+    create(alert: Alert) {
+        this.alerts.push(alert);
+    }
+
+    findAll(): Alert[] {
+        return this.alerts;
+    }
+
+    update(alert: Alert) {
+        this.alerts[alert.id] = alert;
+    }
+}
